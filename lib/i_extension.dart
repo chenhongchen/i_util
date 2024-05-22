1db7fb97ad5c61ecacec8126fb6ef0bad08dad00
chenhongchen <412130100@qq.com>
2024-05-22 13:20:59 +0800

    完善代码

--- lib/i_extension.dart
+++ lib/i_extension.dart
@@ -67,6 +67,14 @@ extension IStringExt on String {
     }
     return 0;
   }
+
+  /// String is in the format "aabbcc" or "ffaabbcc" with an optional leading "#".
+  Color get toColor {
+    final buffer = StringBuffer();
+    if (length == 6 || length == 7) buffer.write('ff');
+    buffer.write(replaceFirst('#', ''));
+    return Color(int.parse(buffer.toString(), radix: 16));
+  }
 }
 
 extension IIntExt on int {
@@ -144,10 +152,7 @@ extension IDoubleExt on double {
 extension IColorExt on Color {
   /// String is in the format "aabbcc" or "ffaabbcc" with an optional leading "#".
   static Color fromHex(String hexString) {
-    final buffer = StringBuffer();
-    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
-    buffer.write(hexString.replaceFirst('#', ''));
-    return Color(int.parse(buffer.toString(), radix: 16));
+    return hexString.toColor;
   }
 
   /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
