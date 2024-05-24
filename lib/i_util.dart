d91d386fc30c1cbb9673f1e9a59a94e71642a70b
chenhongchen <412130100@qq.com>
2024-05-24 11:10:54 +0800

    完善代码

--- lib/i_util.dart
+++ lib/i_util.dart
@@ -5,6 +5,7 @@ export 'router/net_status_manager.dart';
 export 'i_size.dart';
 export 'i_alert.dart';
 export 'i_extension.dart';
+export 'i_logger.dart';
 import 'i_util_platform_interface.dart';
 
 class IUtil {
